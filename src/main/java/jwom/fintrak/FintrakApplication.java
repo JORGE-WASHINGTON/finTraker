6fd22225eab9e223fb727d38623f608101e80ef9
jorge <jwoliveira18@gmail.com>
2023-07-14 21:10:35 +1000

    initializr project scaffolding

---
+++ src/main/java/jwom/fintrak/FintrakApplication.java
@@ -0,0 +1,13 @@
+package jwom.fintrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintrakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jwom/fintrak/FintrakApplicationTests.java
@@ -0,0 +1,13 @@
+package jwom.fintrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
