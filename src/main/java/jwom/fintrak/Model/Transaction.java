70f9cb369563f658197fddd92ea978d73643df12
jorge <jwoliveira18@gmail.com>
2023-07-18 00:36:59 +1000

    added type and accountId fields.

--- src/main/java/jwom/fintrak/Model/Transaction.java
+++ src/main/java/jwom/fintrak/Model/Transaction.java
@@ -1,12 +1,20 @@
 package jwom.fintrak.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,12 +24,21 @@ public class Transaction {
 
     private Double amount;
 
-    private Date date;
+    private LocalDate date;
+
+    private Type type;
+
+    @Column(name = "account_id", insertable = false, updatable = false)
+    private Long accountId;
+
+    public enum Type {
+        EXPENSE,
+        INCOME
+    }
 
     @ManyToOne
     @JoinColumn(name = "account_id")
     private Account account;
 
-    // Add any additional fields, relationships, and Lombok annotations as needed
 }
 
